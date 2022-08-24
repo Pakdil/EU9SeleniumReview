e86043b93d9c15edfed3dd52552c68f3a784c836
Pakdil <eaasbb@gmail.com>
2022-08-24 11:26:21 +0300

    EU9SeleniumReview FirstClass

---
+++ src/test/java/Week1/CheckTitle.java
@@ -0,0 +1,37 @@
+package Week1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckTitle {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/");
+
+        driver.manage().window().maximize();
+
+        Thread.sleep(2000);
+
+        // User stroy
+
+        String title = driver.getTitle();
+
+        if (title.equals("Practice")) {
+            System.out.println("The title is correct");
+        } else {
+            System.out.println("The title is not correct");
+        }
+
+
+        System.out.println("The current URL the page is: "+ driver.getCurrentUrl());
+
+        driver.close();
+    }
+
+}
---
+++ src/test/java/Week1/FirstClass.java
@@ -0,0 +1,44 @@
+package Week1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+       // WebDriverManager.operadriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.facebook.com");
+
+        // go to ebay page
+
+        driver.navigate().to("https://www.ebay.com");
+
+        //go back to facebook
+        driver.navigate().back();
+
+        // go to ebay one more time
+
+        driver.navigate().forward();
+
+        //refresh ebay, still on ebay
+
+        driver.navigate().refresh();
+
+
+        System.out.println(driver.getTitle());
+
+
+
+
+
+    }
+
+
+
+}
