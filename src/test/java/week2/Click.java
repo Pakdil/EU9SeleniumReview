5832a5ba4bd9d6062afea0f0e44361b188e4e074
pakdil <eaasbb@gmail.com>
2022-09-21 13:15:07 +0300

    EU9SeleniumReview SendKeys

--- src/test/java/week2/Click.java
+++ src/test/java/week2/Click.java
@@ -1,4 +1,17 @@
 package week2;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Click {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://practice.cydeo.com/");
+    }
 }
