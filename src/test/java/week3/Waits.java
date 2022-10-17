9154d9a69264b98c7c2969317ee3d5af86948859
pakdil <eaasbb@gmail.com>
2022-10-17 15:53:31 +0300

    EU9SeleniumReview Week3 Display

--- src/test/java/week3/Waits.java
+++ src/test/java/week3/Waits.java
@@ -4,17 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utilities.Driver;
 
+import java.sql.SQLOutput;
+import java.util.concurrent.TimeUnit;
+
 
 public class Waits {
 
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = Driver.getDriver();
 
-        driver.get("https://practice.cydeo.com/dynamic_loading/4");
+//        driver.get("https://practice.cydeo.com/dynamic_loading/4");
+//
+//        Thread.sleep(6000);
+//
+//        System.out.println(driver.findElement(By.xpath("//div[@id=\"finish\"]/h4")).isDisplayed());
 
-        Thread.sleep(6000);
+        driver.get("https://practice.cydeo.com/dynamic_loading/3");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        System.out.println(driver.findElement(By.xpath("//div[@id=\"finish\"]/h4")).isDisplayed());
+        System.out.println(driver.findElement(By.id("login")).getAttribute("name"));
 
 
     }
