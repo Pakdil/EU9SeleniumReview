4b774ef838b2b6f819e0d04d28f2d273d191af0d
pakdil <eaasbb@gmail.com>
2022-10-24 11:58:36 +0300

    ExtraTasks SmartBearUtil

--- src/test/java/ExtraTasks/SmartBearUtil.java
+++ src/test/java/ExtraTasks/SmartBearUtil.java
@@ -2,7 +2,11 @@ package ExtraTasks;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import net.bytebuddy.asm.Advice;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class SmartBearUtil {
 
@@ -18,18 +22,33 @@ public class SmartBearUtil {
                 WebDriverManager.firefoxdriver().setup();
                 break;
             case "safari":
-                    WebDriverManager.safaridriver().setup();
-                    break;
+                WebDriverManager.safaridriver().setup();
+                break;
             case "edge":
-                        WebDriverManager.edgedriver().setup();
-                        break;
+                WebDriverManager.edgedriver().setup();
+                break;
             default:
                 System.out.println("\"Please enter a valid browser name in lowercase\" = " + "Please enter a valid browser name in lowercase");
         }
 
+        driver.manage().window().maximize();
+
+        WebElement userName = driver.findElement(By.xpath("//input[@name='ctl00$MainContent$username']"));
+        WebElement password = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_password']"));
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_login_button']"));
+
+        userName.sendKeys("Tester");
+        password.sendKeys("test");
+        loginBtn.click();
+
+        List<WebElement> links = driver.findElements(By.xpath("//a"));
+        System.out.println("links cont = " + links.size());
+
+        for (WebElement link : links) {
+            System.out.println("link text = " + link.getText());
+
+        }
+
+
     }
-
-
-
-
 }
