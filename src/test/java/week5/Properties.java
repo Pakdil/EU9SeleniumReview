2c9ba640ba6a865c59064736383299e2e59d3ba1
pakdil <eaasbb@gmail.com>
2022-10-28 22:26:52 +0300

    week5 Properties

---
+++ src/test/java/week5/Properties.java
@@ -0,0 +1,18 @@
+package week5;
+
+import org.testng.annotations.Test;
+
+public class Properties {
+
+    @Test
+    public void test1() {
+
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getProperty("java.compiler"));
+
+
+    }
+
+
+}
