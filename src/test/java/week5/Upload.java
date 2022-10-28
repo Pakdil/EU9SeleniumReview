15c2cd5ec48d71e7d4437772b4294ac7404f42ca
pakdil <eaasbb@gmail.com>
2022-10-28 23:38:31 +0300

    week5 upload

---
+++ src/test/java/week5/Upload.java
@@ -0,0 +1,22 @@
+package week5;
+
+import base.TestBase;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Upload extends TestBase {
+
+    @Test
+    public void uploadTest() {
+        driver.get("http://practice.cybertekschool.com/upload");
+        String path = "F:\\Pakdil\\EU9SeleniumReview\\src\\test\\java\\utilities\\upload.txt";
+        driver.findElement(By.id("file-upload")).sendKeys(path);
+        driver.findElement(By.id("file-submit")).click();
+
+        String actual = driver.findElement(By.id("uploaded-files")).getText();
+        String expected = "upload.txt";
+
+        Assert.assertEquals(actual, expected);
+    }
+}
