d13752eca2e0d76af52dc42cf71be0b59e900fa3
pakdil <eaasbb@gmail.com>
2022-10-29 19:13:40 +0300

    week6 LoginTest

--- src/test/java/week6/pages/tests/LoginTest.java
+++ src/test/java/week6/pages/tests/LoginTest.java
@@ -1,7 +1,32 @@
 package week6.pages.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.Driver;
+import week6.pages.LoginPage;
+
+import java.util.concurrent.TimeUnit;
+
 public class LoginTest {
 
-    
+    WebDriver driver;
+    LoginPage loginPage;
+    @BeforeMethod
+    public void setUp() {
+        driver = Driver.getDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        loginPage = new LoginPage();
+
+    }
+
+    @Test
+    public void loginTest() {
+        String username = "tomsmith";
+        String password = "SuperSecretPassword";
+        loginPage.loginMethod(username, password);
+    }
+
 
 }
